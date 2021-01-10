48c345d1d3dacab59a0408c021f6387805840a94
Alexander Ignatov <yalishanda@abv.bg>
2021-01-10 23:58:42 +0200

    Add some social buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import CookieNotice from 'react-cookienotice'
+import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Сайт</h1>
+        <div>
+          <SocialIcon className="socialicon" url="https://github.com/allexks"/>
+          <SocialIcon className="socialicon" url="https://www.linkedin.com/in/alexander-ignatov-053b53b3/"/>
+          <SocialIcon className="socialicon" url="https://reddit.com/u/allexks"/>
+        </div>
       </header>
       <CookieNotice
         acceptButtonLabel="Разбрах"
